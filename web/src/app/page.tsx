8a9db486bc09bbc890a5f8be802ed8c2886f97bb
Cyrus Nouroozi <cyrus@edendaolab.com>
2024-01-30 22:56:45 -0800

    Fix build issue

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return null
+  return (
+    <div
+      style={{ marginTop: "20dvh", marginLeft: "auto", marginRight: "auto" }}
+    >
+      <h1 style={{ fontSize: 64 }}>LFG</h1>
+    </div>
+  )
 }
